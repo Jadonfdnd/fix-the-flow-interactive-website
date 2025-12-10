262fe94f326e555b6008869d1ce7052dda7300e2
Jadonfdnd <jadon.tjon@hva.nl>
2025-12-10 10:46:02 +0100

    added heart icon animations #2
    not working #2

--- scripts/test.js
+++ scripts/test.js
@@ -1,10 +1,13 @@
-//Select : button
-let heartButton = document.querySelector ('.heart')
-//event listener 
-heartButton.addEventListener('click', toggleLike);
+//Select : heart button 
+const heartButton = document.querySelector('.like-button')
+console.log(heartButton)
+//add click event listener and use toggleLike Function
+heartButton.addEventListener('click', toggleLike)
 
 //function toggleLike
-function toggleLike() {
+function toggleLike(event) {
     heartButton.classList.toggle("liked");
+    
+    
 }
 //wait on click so heart gets animation/heart color change to red
