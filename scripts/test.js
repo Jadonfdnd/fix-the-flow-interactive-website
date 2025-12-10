7216930f91d20089fc0326ed398ca6654024ad7b
Jadonfdnd <jadon.tjon@hva.nl>
2025-12-10 10:58:51 +0100

    Fixed heart button #2

--- scripts/test.js
+++ scripts/test.js
@@ -1,5 +1,5 @@
 //Select : heart button 
-const heartButton = document.querySelector('.like-button')
+const heartButton = document.querySelector('button[class="like-button"]')
 console.log(heartButton)
 //add click event listener and use toggleLike Function
 heartButton.addEventListener('click', toggleLike)
@@ -7,7 +7,7 @@ heartButton.addEventListener('click', toggleLike)
 //function toggleLike
 function toggleLike(event) {
     heartButton.classList.toggle("liked");
-    
+    event.preventDefault()
     
 }
 //wait on click so heart gets animation/heart color change to red
